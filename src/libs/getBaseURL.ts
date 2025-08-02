26e316d97f0b4c126c30e02d8ded9ea22dc6833e
Jhon Briones <joanbric@gmail.com>
2025-08-02 01:27:35 +0100

    feat: implement URL shortener functionality with clipboard support and environment configuration

---
+++ src/libs/getBaseURL.ts
@@ -0,0 +1,7 @@
+const { PUBLIC_VERCEL_ENV, PUBLIC_VERCEL_PROJECT_PRODUCTION_URL } = import.meta
+	.env;
+
+export const BASE_URL =
+	PUBLIC_VERCEL_ENV === 'local'
+		? 'http://localhost:4321'
+		: `https://url.${PUBLIC_VERCEL_PROJECT_PRODUCTION_URL}`;
