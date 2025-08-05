c2de686f18c34b7cbcef1e8987718b01640fa8d4
Jhon Briones <joanbric@gmail.com>
2025-08-05 05:29:02 +0100

    fix: format menuItems array for consistency

--- src/libs/collections.ts
+++ src/libs/collections.ts
@@ -1,14 +1,14 @@
 export const menuItems = [
-	{
-		id: 'customHash',
-		label: 'Use Custom Hash'
-	},
-	{
-		id: 'authCode',
-		label: 'Use Authentication Code'
-	},
-	{
-		id: 'showLinks',
-		label: 'Show Last Links'
-	}
-];
+  {
+    id: 'customHash',
+    label: 'Use Custom Hash'
+  },
+  {
+    id: 'authCode',
+    label: 'Use Authentication Code'
+  },
+  {
+    id: 'showLinks',
+    label: 'Show Last Links'
+  }
+]
